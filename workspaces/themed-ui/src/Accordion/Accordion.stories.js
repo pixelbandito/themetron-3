0f267431ec4d6f45c18eb360c94a5d285ba9683d
Chris Garcia <pixelbandito@gmil.com>
2020-08-06 23:35:48 -0500

    docs: Added stories and screen tests for Button, updated screen tests for Accordion.

--- workspaces/themed-ui/src/Accordion/Accordion.stories.js
+++ workspaces/themed-ui/src/Accordion/Accordion.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 /* eslint-disable import/no-extraneous-dependencies */
@@ -12,13 +12,73 @@ import {
 
 import { defaultTheme } from '@pixelbandito/theme';
 import Font from '../Font';
-import Accordion from '.';
+import Accordion, { AccordionContext } from '.';
+
+const CustomHead = () => {
+  const accordionContext = useContext(AccordionContext);
+
+  return (
+    <div
+      style={{
+        color: accordionContext ? 'red' : 'blue',
+      }}
+    >
+      Red when accordion is open.
+    </div>
+  );
+};
+
+export const Reference = () => (
+  <Accordion
+    className={text('className', '')}
+    data-testid={text('data-testid', 'accordion-test')}
+    defaultIsOpen={boolean('defaultIsOpen', true)}
+    speed={number('speed', 1)}
+    variant={text('variant', 'default')}
+  >
+    {text('children', 'Children')}
+  </Accordion>
+);
+
+export const Examples = () => (
+  <>
+    <Accordion
+      m="md"
+    >
+      Default (1 second per 1000 pixel height)
+    </Accordion>
+    <Accordion
+      m="md"
+      variant="primary"
+    >
+      Primary variant
+    </Accordion>
+    <Accordion
+      m="md"
+      speed={0.1}
+    >
+      Slower speed (0.1 seconds per 1000 pixels)
+    </Accordion>
+    <Accordion
+      defaultIsOpen
+      m="md"
+    >
+      Default is open
+    </Accordion>
+    <Accordion
+      head={<CustomHead />}
+      m="md"
+    >
+      Custom head using accordion context
+    </Accordion>
+  </>
+);
 
 export default {
   title: 'Accordion',
   component: Accordion,
   decorators: [
-    withKnobs,
+    withKnobs({ escapeHTML: false }),
     storyFn => <Font tag="div">{storyFn()}</Font>,
     storyFn => (
       <ThemeProvider
@@ -35,15 +95,3 @@ export default {
     ),
   ],
 };
-
-export const Reference = () => (
-  <Accordion
-    className={text('className', '')}
-    defaultIsOpen={boolean('defaultIsOpen', true)}
-    speed={number('speed', 1)}
-    variant={text('variant', 'default')}
-    data-testid={text('data-testid', 'accordion-test')}
-  >
-    {text('children', 'Children')}
-  </Accordion>
-);
